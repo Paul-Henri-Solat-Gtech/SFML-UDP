ae786cdc4caf22f26b776b84d6c15052a57fc003
Paul-Henri-Solat-Gtech <psolat@gaming.tech>
2025-01-17 12:58:47 +0100

    Threads

--- SFM-UDP/src/Client/main.cpp
+++ SFM-UDP/src/Client/main.cpp
@@ -4,17 +4,16 @@
 #include "Ball.h"
 #include "Network.h"
 
-int main()
+// UDP NETWORK
+DWORD WINAPI Static_Udp(void* pParam)
 {
     std::cout << "Hello, Im Client!\n";
-
-    // ------------------ UDP
 
     Network::InitDLL();
 
     Network* client = new Network;
 
-    if (client->InitSocket() != true) 
+    if (client->InitSocket() != true)
     {
         std::cout << "Cannot create socket\n";
     }
@@ -34,19 +33,25 @@ int main()
         WSACleanup();
         return 1;
     }
-    
+
     // add threads + while
     client->SendMsgTo(servAddr->GetAdress());
     client->RecieveMsg();
 
-    // ------------------ SFML
+    client->UninInitNetwork();
 
+    return 0;
+}
+
+// SFML RENDER
+DWORD WINAPI Static_Sfml(void* pParam)
+{
     // Window
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
     // Objects
     Ball* ball = new Ball(50, window.getSize().x / 2, window.getSize().y / 2, sf::Color::Magenta);
-    Ball* centerBall = new Ball(10,0,0, sf::Color::White);
+    Ball* centerBall = new Ball(10, 0, 0, sf::Color::White);
 
     ball->SetSpeed(0.02f);
     //std::cout << "posxy:" << ball->GetPosX() << " " << ball->GetPosY();
@@ -66,7 +71,7 @@ int main()
         // Update
         ball->Update();
         centerBall->SetPosition(ball->GetPosX(), ball->GetPosY());
-        
+
         // Clear
         window.clear(sf::Color::Black);
 
@@ -78,7 +83,52 @@ int main()
         window.display();
     }
 
-    client->UninInitNetwork();
+    return 0;
+}
+
+int main()
+{
+    // ------------------ THREADS
+    // Tableau de ThreadId static
+    const int numberOfThreads = 2;
+    HANDLE ThreadHandles[numberOfThreads];
+    DWORD threadId[numberOfThreads];
+
+    //ThreadHandles[id] = CreateThread(nullptr, 0, fctnName, param, 0, &threadId[id]);
+    
+    // ------------------ UDP
+    ThreadHandles[0] = CreateThread(nullptr, 0, Static_Udp, 0, 0, &threadId[0]);
+
+    // ------------------ SFML
+    ThreadHandles[1] = CreateThread(nullptr, 0, Static_Sfml, 0, 0, &threadId[1]);
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (ThreadHandles[i] == nullptr)
+        {
+            std::cerr << "Erreur pendant la creation du thread!" << std::endl;
+            return 1;
+        }
+    }
+
+    // Verifie si les threads on finis de sexecuter
+    WaitForMultipleObjects(numberOfThreads, ThreadHandles, true, INFINITE);
+
+    // Ferme les Threads apres execution de celles-ci
+    for (int i = 0; i < numberOfThreads; i++)
+    {
+        CloseHandle(ThreadHandles[i]);
+    }
+
+
+
+
+
+
+
+   
+
+
 
     return 0;
 }
